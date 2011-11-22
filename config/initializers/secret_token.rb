48908c535dff2db43fb848037fa074e0883c209a
mikeebert <mike@mikeebert.net>
2011-11-22 14:31:48 -0600

    Ebert first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasySked::Application.config.secret_token = 'd60848e4d9dfb8919f34cda95e605f126ba1100d165e4ab734c5fb61d90117768bf820f7bc66cc5b8421abe93fc6755298ea2aaaea6b7318cc1407db0e0d86f5'
